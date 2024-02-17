5e8d80ef74564bc9b46d7a8a5f8aa055d6f91b2c
Piyush Rebadiya <piyushrebadiya123@gmail.com>
2024-02-17 13:04:40 +0530

    add dynamic folder and files path get

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const ngrok = require('ngrok');
 
 const app = express();
 
@@ -9,16 +10,14 @@ function getAllPDFFiles(directory) {
 
     // Function to recursively search for PDF files
     function findPDFFiles(dir) {
-        console.log('dir123123==>>>', dir)
-        console.log('dir replaceAll ==>>>', dir.replaceAll(/\\/g, '/'))
+        console.log('dir123123==>>>123', dir)
         // const dynamicPath = dir.slice(39).replaceAll(/\\/g, '/');
         const replacePath = dir.replaceAll(/\\/g, '/');
-        console.log('replacePath', replacePath)
         const findPath = replacePath.lastIndexOf('pdf');
-        console.log('findPath', findPath)
         const dynamicPath = replacePath.slice(findPath);
         console.log('dynamicPath', dynamicPath)
         const files = fs.readdirSync(dir);
+        console.log('files1232', files)
         files.forEach(file => {
             const filePath = path.join(dir, file);
             const stat = fs.statSync(filePath);
@@ -41,6 +40,41 @@ function getAllPDFFiles(directory) {
 
     return pdfFiles;
 }
+function getAllPDF(directory) {
+    let pdfFiles = [];
+    function findPDFFiles(dir) {
+        const replacePath = dir.replaceAll(/\\/g, '/');
+        const findPath = replacePath.lastIndexOf('pdf');
+        const dynamicPath = replacePath.slice(findPath);
+        const files = fs.readdirSync(dir);
+        files.forEach(file => {
+            const filePath = path.join(dir, file);
+            console.log('filePath', filePath)
+            const stat = fs.statSync(filePath);
+            console.log('stat', stat)
+            console.log('path.extname(file)', path.extname(file))
+            if (stat.isDirectory() && !path.extname(file)) {
+                console.log('file', file);
+                pdfFiles.push({
+                    title: file.toLocaleLowerCase(),
+                    path: dynamicPath,
+                    birthtime: stat.birthtime,
+                    type: 'folder'
+                });
+            } else if (path.extname(file).toLowerCase() === '.pdf') {
+                pdfFiles.push({
+                    filename: file.toLocaleLowerCase(),
+                    title: file.toLocaleLowerCase()?.slice(0, -4),
+                    path: dynamicPath,
+                    birthtime: stat.birthtime,
+                    type: 'pdf'
+                });
+            }
+        });
+    }
+    findPDFFiles(directory);
+    return pdfFiles;
+}
 
 app.get('/download-pdf', (req, res) => {
     const directory = path.join(__dirname, 'pdf');
@@ -61,7 +95,9 @@ app.get('/download-pdf', (req, res) => {
 
     console.log(latestObject);
     const pdfFilePath = path.join(__dirname, latestObject?.path || '', latestObject?.filename || '');
+    console.log('pdfFilePath', pdfFilePath)
     const fileStream = fs.createReadStream(pdfFilePath);
+    // console.log('fileStream', fileStream)
     // Listen for errors on the file stream
     fileStream.on('error', (err) => {
         console.error('Error reading PDF file:', err);
@@ -71,8 +107,158 @@ app.get('/download-pdf', (req, res) => {
     fileStream.pipe(res);
 });
 
+const onlyFileName = (fileName, res) => {
+    const directory = path.join(__dirname, 'pdf');
+    const pdfFiles = getAllPDFFiles(directory);
+    console.log('pdfFiles', pdfFiles)
+    const findPdf = pdfFiles.filter((file) => file.filename === `${fileName}.pdf`);
+    let latestBirthtime = 0;
+    let latestObject = null;
+
+    // Iterate through the array to find the latest birthtime
+    findPdf?.forEach(obj => {
+        const birthtime = new Date(obj.birthtime).getTime();
+        if (birthtime > latestBirthtime) {
+            latestBirthtime = birthtime;
+            latestObject = obj;
+        }
+    });
+
+    console.log(latestObject);
+    const pdfFilePath = path.join(__dirname, latestObject?.path || '', latestObject?.filename || '');
+    console.log('pdfFilePath', pdfFilePath)
+    const fileStream = fs.createReadStream(pdfFilePath);
+    // console.log('fileStream', fileStream)
+    // Listen for errors on the file stream
+    fileStream.on('error', (err) => {
+        console.error('Error reading PDF file:', err);
+        res.status(404).send('File not found');
+    });
+    // res.setHeader('Content-Disposition', 'attachment; filename="AgentList.pdf"');
+    fileStream.pipe(res);
+}
+const onlyFolderName = async (folderName, res) => {
+
+    try {
+
+        console.log('folderName', folderName)
+        const directory = path.join(__dirname, folderName);
+        console.log('directory', directory)
+        const pdfFiles = getAllPDF(directory);
+        console.log('pdfFiles', pdfFiles)
+        // const collection = getDynamicDbName(req , req.rawHeaders[1]).collection('product');
+        // const filter = {};
+        // const result = await collection.find(filter).toArray();
+        res.json(pdfFiles);
+    } catch (error) {
+        console.error('Error fetching profiles:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    // const pdfFiles = getAllPDFFiles(directory);
+    // console.log('pdfFiles', pdfFiles)
+    // const findPdf = pdfFiles.filter((file) => file.filename === `${fileName}.pdf`);
+    // let latestBirthtime = 0;
+    // let latestObject = null;
+
+    // // Iterate through the array to find the latest birthtime
+    // findPdf?.forEach(obj => {
+    //     const birthtime = new Date(obj.birthtime).getTime();
+    //     if (birthtime > latestBirthtime) {
+    //         latestBirthtime = birthtime;
+    //         latestObject = obj;
+    //     }
+    // });
+
+    // console.log(latestObject);
+    // const pdfFilePath = path.join(__dirname, latestObject?.path || '', latestObject?.filename || '');
+    // console.log('pdfFilePath', pdfFilePath)
+    // const fileStream = fs.createReadStream(pdfFilePath);
+    // // console.log('fileStream', fileStream)
+    // // Listen for errors on the file stream
+    // fileStream.on('error', (err) => {
+    //     console.error('Error reading PDF file:', err);
+    //     res.status(404).send('File not found');
+    // });
+    // // res.setHeader('Content-Disposition', 'attachment; filename="AgentList.pdf"');
+    // fileStream.pipe(res);
+}
+
+const bothFileNameAndFolderName = (filename, folderpath, res) => {
+    const directory = path.join(__dirname).replaceAll(/\\/g, '/');
+    const pdfFilePath = `${directory}/${folderpath}/${filename}.pdf`;
+    const fileStream = fs.createReadStream(pdfFilePath);
+    fileStream.on('error', (err) => {
+        console.error('Error reading PDF file:', err);
+        res.status(404).send('File not found');
+    });
+    // res.setHeader('Content-Disposition', 'attachment; filename="AgentList.pdf"');
+    fileStream.pipe(res);
+}
+
+// const dummyFun  = (folderName) => {
+//     const directory = path.join(__dirname, folderName);
+//     const pdfFiles = [];
+//     console.log('directory123', directory);
+//     function findPDFFiles(dir, folderName) {
+//         console.log('dir123123==>>>123', dir)
+//         // console.log('dir replaceAll ==>>>', dir.replaceAll(/\\/g, '/'))
+//         // const dynamicPath = dir.slice(39).replaceAll(/\\/g, '/');
+//         const replacePath = dir.replaceAll(/\\/g, '/');
+//         // console.log('replacePath', replacePath)
+//         const findPath = replacePath.lastIndexOf('pdf');
+//         // console.log('findPath', findPath)
+//         const dynamicPath = replacePath.slice(findPath);
+//         // console.log('dynamicPath', dynamicPath)
+//         const files = fs.readdirSync(dir);
+//         console.log('files1232', files)
+//         files.forEach(file => {
+//             const filePath = path.join(dir, file);
+//             const stat = fs.statSync(filePath);
+//             if (stat.isDirectory()) {
+//                 console.log('dynamicPath==>>> ',dynamicPath.split('/')[dynamicPath.split('/').length - 1] === folderName);
+//                 console.log('folderName', folderName)
+//                 // Recursively search subdirectories
+//                 if(dynamicPath.split('/')[dynamicPath.split('/').length - 1] !== folderName){ 
+//                     findPDFFiles(filePath, folderName);
+//                 }
+//             } else if (path.extname(file).toLowerCase() === '.pdf') {
+//                 // Found a PDF file, add it to the list
+//                 pdfFiles.push({
+//                     filename: file.toLocaleLowerCase(),
+//                     path: dynamicPath,
+//                     birthtime: stat.birthtime
+//                 });
+//             }
+//         });
+//     }
+
+//     // Start the recursive search
+//     findPDFFiles(directory, folderName);
+//     return pdfFiles;
+// }
+// console.log('===>>> ',dummyFun('testing'));
+
+app.get('/download-folder', (req, res) => {
+    console.log('req.query', req.query)
+    const { filename, folderpath } = req.query
+    if (filename && folderpath) {
+        bothFileNameAndFolderName(filename, folderpath, res)
+    } else if (filename && !folderpath) {
+        onlyFileName(filename, res)
+    } else if (!filename && folderpath) {
+        onlyFolderName(folderpath, res)
+    } else {
+        res.status(404).send('something went wrong');
+    }
+});
+
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    // ngrok.connect(PORT).then(ngrokUrl => {
+    //     console.log(`Ngrok tunnel in: ${ngrokUrl}`); // Log the ngrok URL once the tunnel is established
+    // }).catch(error => {
+    //     console.log(`Couldn't tunnel ngrok: ${error}`); // Log an error if ngrok fails to start
+    // });
 });
